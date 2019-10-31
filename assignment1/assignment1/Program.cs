a6add51557eb27f26ae426ba826cad7e6a0827b9
zuhaq21 <57126201+zuhaq21@users.noreply.github.com>
2019-10-31 20:03:33 +0500

    Add files via upload

---
+++ assignment1/assignment1/Program.cs
@@ -0,0 +1,70 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student obj = new Student();
+            char exitChoice;
+            int choice;
+            do
+            {
+                String name, semester, cgpa, department, university, id;
+                Console.Write("Press 1 to add student.\n");
+                Console.Write("Press 2 to search for student. \n");
+                Console.Write("Press 3 to delete a record of a student. \n");
+                Console.Write("Press 4 to check top 4 students of class.\n");
+                Console.Write("Press 5 to mark attendence.\n");
+                Console.Write("Press 6 to view attendence\n");
+                Console.Write("\t\t\t Enter your choice  \n");
+                choice = Convert.ToInt32(Console.ReadLine());
+                if (choice == 1)
+                {
+                    Console.Write("Enter the Name Of Student:  ");
+                    name = Convert.ToString(Console.ReadLine());
+                    Console.Write("Enter the semester of student:  ");
+                    semester = Convert.ToString(Console.ReadLine());
+                    Console.Write("Enter the cgpa of student:  ");
+                    cgpa = Convert.ToString(Console.ReadLine());
+                    Console.Write("Enter the department of student:  ");
+                    department = Convert.ToString(Console.ReadLine());
+                    Console.Write("Enter the university of student:  ");
+                    university = Convert.ToString(Console.ReadLine());
+                    id = obj.setId();
+                    obj.addStudent(name, semester, cgpa, department, university, id);
+
+                }
+                else if (choice == 2)
+                {
+
+                }
+                else if (choice == 3)
+                {
+
+                }
+                else if (choice == 4)
+                {
+
+                }
+                else if (choice == 5)
+                {
+
+                }
+                else
+                {
+
+                }
+                choice = 0;
+                Console.WriteLine("Want to do again: y/n");
+                exitChoice = Convert.ToChar(Console.Read());
+            }
+            while (exitChoice == 'y' || exitChoice == 'Y');
+        }
+    }
+}
---
+++ assignment1/assignment1/Student.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace assignment1
+{
+    class Student
+    {
+        public void addStudent(String name, String semester, String cgpa, String department, String university, String id)
+        {
+            using(StreamWriter writetext = File.AppendText("D:/Uni/BSE-5/VP/Assignments/Students.txt"))
+            {
+                String attendence = "A";
+                writetext.WriteLine(name);
+                writetext.WriteLine(id);
+                writetext.WriteLine(semester);
+                writetext.WriteLine(cgpa);
+                writetext.WriteLine(department);
+                writetext.WriteLine(university);
+                writetext.WriteLine(attendence);
+            }
+        }
+
+        public string setId()
+        {
+            byte[] buffer = Guid.NewGuid().ToByteArray();
+            var FormNumber = BitConverter.ToUInt32(buffer, 0) ^ BitConverter.ToUInt32(buffer, 4) ^ BitConverter.ToUInt32(buffer, 8) ^ BitConverter.ToUInt32(buffer, 12);
+            return FormNumber.ToString("X");
+        }
+
+        //public void deleteStudent()
+        //{
+
+        //}
+        //public void searchById()
+        //{
+
+        //}
+        //public void searchByName()
+        //{
+
+        //}
+        //public void displayStudents()
+        //{
+
+        //}
+        //public void displayTopThree()
+        //{
+
+        //}
+    }
+}
